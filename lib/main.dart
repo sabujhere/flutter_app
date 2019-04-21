cb9b41f5ada9a5d1029d6c9d7530394befad4bfa
sa991288 <sabujece@gmail.com>
2019-04-21 09:56:32 -0500

    Modifying State

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,45 @@ class TodoList extends StatefulWidget{
   createState()=> new TodoListState();
 }
 class TodoListState extends State<TodoList>{
+  List<String> _todoItems = [];
+
+  // This will be called each time the + button is pressed
+  void _addTodoItem(){
+    // Putting code inside "setState" tells the app that our state has changed, and
+    // it will automatically re-render the list
+    setState(
+        () {
+          int index = _todoItems.length;
+          _todoItems.add('Item' + index.toString());
+        });
+  }
+
+  Widget _buildTodoList(){
+    return new ListView.builder(
+        itemBuilder: (context, index){
+         if(index <_todoItems.length){
+           return _buildTodoItem(_todoItems[index]);
+         }
+        });
+  }
+
+  Widget _buildTodoItem(String todoText){
+    return new ListTile(
+      title: new Text(todoText),
+    );
+  }
+
   @override
   Widget build(BuildContext context){
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Todo List'),
       ),
+      body: _buildTodoList(),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: _addTodoItem,
+      tooltip: 'Add Task',
+      child: new Icon(Icons.add),),
     );
   }
 }
