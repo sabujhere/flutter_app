192e8037727f4fc1df5a0c95b5ce2a76fefe2ab9
sa991288 <sabujece@gmail.com>
2019-04-21 09:25:31 -0500

    app ready to be stateful

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,21 @@ class TodoApp extends StatelessWidget{
   Widget build(BuildContext context){
     return new MaterialApp(
       title: 'Todo List',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Todo List'),
-        ),
-      )
+      home: new TodoList()
+    );
+  }
+}
+class TodoList extends StatefulWidget{
+  @override
+  createState()=> new TodoListState();
+}
+class TodoListState extends State<TodoList>{
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Todo List'),
+      ),
     );
   }
 }
